39878dbec4f43e702bd4b2dab92af4aaeb71efd9
kimsungil <kittoboy@naver.com>
2016-07-19 08:21:54 +0900

    Toolbar 적용 완료.

--- app/src/main/java/com/dev_daddy/appbarexam/MainActivity.java
+++ app/src/main/java/com/dev_daddy/appbarexam/MainActivity.java
@@ -1,7 +1,10 @@
 package com.dev_daddy.appbarexam;
 
+import android.graphics.Color;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar)findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 }
