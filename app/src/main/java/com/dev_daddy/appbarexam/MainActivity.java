cad8cb5be463d686f9a7f9f28db0d995d4fb1093
kimsungil <kittoboy@naver.com>
2016-07-19 08:55:30 +0900

    메인화면 Toolbar 아이템 추가 및 이벤트 처리 완료.

--- app/src/main/java/com/dev_daddy/appbarexam/MainActivity.java
+++ app/src/main/java/com/dev_daddy/appbarexam/MainActivity.java
@@ -1,10 +1,11 @@
 package com.dev_daddy.appbarexam;
 
-import android.graphics.Color;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar)findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_items_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                Toast.makeText(this, "Clicked settings action item.", Toast.LENGTH_LONG).show();
+                return true;
+
+            case R.id.action_favorite:
+                Toast.makeText(this, "Clicked favorite action item.", Toast.LENGTH_LONG).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
